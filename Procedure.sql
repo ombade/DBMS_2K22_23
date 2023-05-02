b0dcd9fea1271b718a674203e39307f935d0e7d4
Bade om <71856738+ombade@users.noreply.github.com>
2023-05-02 12:49:32 +0530

    the Basic of the Stored Procedure syntax and examples are added

--- Procedure.sql
+++ Procedure.sql
@@ -1,9 +1,33 @@
-Delimiter $$
-create procedure doc_(IN i int)
-Begin
-Declare T Decimal(6,2); 
-Select Dconstultantfee into T from DOCTOR where Did=i;
-set T=T+T*0.1;
-update DOCTOR set Dconstultantfee= T where Did=i;
-end $$
-Delimiter ;
+-- creating the simple PROCEDURE
+DELIMITER //
+create Procedure Reader_number()
+BEGIN
+select Rid ,Rname
+from reader ;
+end //
+DELIMITER ;
+
+-- call the above PROCEDURE
+CALL Reader_number();
+
+-- creating the Parameterized Procedure
+
+DELIMITER //
+create Procedure reader_age(name varchar(30))
+BEGIN
+select * from reader where Rname = name;
+End //
+DELIMITER ;
+
+-- Multiple Parameterized Procedures
+delimiter //
+create PROCEDURE book_info(b_name varchar(30),price int)
+BEGIN
+select * from Books where bname = b_name AND price =Price;
+end //
+delimiter ;
+
+call book_info('wings of fire',200);
+
+-- to drop the PROCEDURE
+DROP PROCEDURE book_info;
--- views.sql
+++ views.sql
@@ -2,3 +2,15 @@ CREATE view big_reader as
 select * from reader where Rage >=20;
 
 select * from big_reader;
+
+-- update the view 
+CREATE OR REPLACE VIEW big_reader AS
+SELECT Rname ,Rage
+FROM reader
+ where Rage >=20;
+ALTER VIEW big_reader AS    
+SELECT Rname    
+FROM reader    
+ where Rage >=20;  
+--  to drop the view/
+DROP VIEW big_reader;
